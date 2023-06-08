cffea25aeb173e6b13207980cfbca42219b7367c
jhowf <jonathanferreirar@gmail.com>
2023-06-08 15:39:11 -0300

    sparkles: feat: Navbar page

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -1,5 +1,25 @@
 import React from "react";
 
+import { BsCartFill } from "react-icons/bs";
+
+import "./navbar.css";
+
+import Logo from "../assets/Logo.svg";
+
 export const Navbar: React.FC = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex relative justify-between items-center mx-[5rem] my-5">
+      <div className="flex gap-2 items-center">
+        <img className="w-10" src={Logo} alt="Logo TechLong" />
+        <h3>TechLong</h3>
+      </div>
+      <ul className="flex gap-9 items-center  ">
+        <li className="text-white ">Products</li>
+        <li className="text-white ">Contact</li>
+        <li className="text-white text-[20px] ml-[100px] ">
+          <BsCartFill />
+        </li>
+      </ul>
+    </nav>
+  );
 };
